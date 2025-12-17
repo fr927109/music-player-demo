ee4f6dc6932ffde8b1022ac336b1148fb17efbc5
fr927109 <fr927109@gmail.com>
2025-12-16 21:24:43 -0600

    Add OPTIONS preflight handler for CORS

--- music-player-demo/backend/app.py
+++ music-player-demo/backend/app.py
@@ -15,6 +15,18 @@ def is_allowed_origin(origin):
     ]
     return any(re.match(pattern, origin) for pattern in allowed_patterns)
 
+@app.before_request
+def handle_preflight():
+    if request.method == 'OPTIONS':
+        origin = request.headers.get('Origin')
+        if origin and is_allowed_origin(origin):
+            response = jsonify({'status': 'ok'})
+            response.headers['Access-Control-Allow-Origin'] = origin
+            response.headers['Access-Control-Allow-Credentials'] = 'true'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+            return response, 200
+
 @app.after_request
 def after_request(response):
     origin = request.headers.get('Origin')
